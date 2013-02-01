45efec1b03cdd186816f4be153a413c55627cfff
Sebastian Wyder <sebastian.wyder@me.com>
2013-02-01 02:12:31 +0100

    Changed tests

--- tests.php
+++ tests.php
@@ -22,21 +22,38 @@
 function assert_equals($is, $should) {
   if($is != $should) {
     exit(1);
+  } else {
+    println('Passed!');
   }
 }
 
 function assert_url($is) {
   if(!preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $is)) {
     exit(1);
+  } else {
+    println('Passed!');
   }
+}
+
+function println($text) {
+  echo $text . "\n";
 }
 
 require 'Googl.class.php';
 
 $googl = new Googl();
 
+println('#1 - Assert that shortening http://www.google.ch results in an URL');
 assert_url($googl->shorten('http://www.google.ch'));
-assert_equals($googl->expand('http://goo.gl/fbsS'), 'http://www.google.com');
 
+println('#2 - Assert that expanding http://goo.gl/KSggQ resolves to http://www.google.com/');
+assert_equals($googl->expand('http://goo.gl/KSggQ'), 'http://www.google.com/');
+
+println('#3 - Assert that shortening https://www.facebook.com results in an URL');
 assert_url($googl->shorten('https://www.facebook.com'));
+
+println('#4 - Assert that expanding http://goo.gl/wCWWd resolves to http://www.php.net/');
 assert_equals($googl->expand('http://goo.gl/wCWWd'), 'http://www.php.net/');
+
+# If this point is reached, all tests have passed
+println('All tests have successfully passed!');
